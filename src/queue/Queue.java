f30626e6092352a8cfae4928b45a43a25e9bf322
Philip <ajibolaphilip10@gmail.com>
2024-02-25 21:47:42 -0800

    update

--- src/queue/Queue.java
+++ src/queue/Queue.java
@@ -2,4 +2,21 @@ package queue;
 
 public class Queue {
 
+
+    private final String[] element;
+    private int counter ;
+
+    public Queue(int size){
+        this.element = new String[size];
+    }
+
+    public boolean offer(String name) {
+        if(name==null)throw new NullPointerException("NullPointerException: Element isNull");
+        if(counter<element.length) {
+            element[counter] = name;
+            counter++;
+            return true;
+        }
+        else return false;
+    }
 }
--- test/queue/QueueTest.java
+++ test/queue/QueueTest.java
@@ -1,7 +1,36 @@
 package queue;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class QueueTest {
 
+    @Test
+    public void testThatICanOfferAQueue(){
+        Queue queue = new Queue(3);
+        assertTrue(queue.offer("name"));
+    }
+    @Test
+    public void testThatICanOfferMoreElement(){
+        Queue queue = new Queue(3);
+        assertTrue(queue.offer("name"));
+        assertTrue(queue.offer("name1"));
+        assertTrue(queue.offer("name2"));
+    }
+    @Test
+    public void testThatWhenTheQueueCapacityIsFullItReturnFalse(){
+        Queue queue = new Queue(3);
+        assertTrue(queue.offer("name"));
+        assertTrue(queue.offer("name1"));
+        assertTrue(queue.offer("name2"));
+        assertFalse(queue.offer("name2"));
+    }
+    @Test
+    public void testThatWhenNullIsAdded_NullPointerExceptionIsThrown(){
+        Queue queue = new Queue(3);
+        assertTrue(queue.offer("name"));
+        assertTrue(queue.offer("name1"));
+        assertThrows(NullPointerException.class,()-> queue.offer(null));
+    }
 }
