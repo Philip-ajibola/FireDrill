bedefc755350260cdbddd968ee6d50c179cc7f70
Philip <ajibolaphilip10@gmail.com>
2024-02-27 00:27:34 -0800

    update

--- src/shape/threeDimensionalShape/Tetrahedron.java
+++ src/shape/threeDimensionalShape/Tetrahedron.java
@@ -1,4 +1,15 @@
 package shape.threeDimensionalShape;
 
-public class Tetrahedron {
+public class Tetrahedron extends ThreeDimensionalShape{
+    private double lengthOfEdges;
+
+    @Override
+    public double getVolume(){
+        return (Math.pow(lengthOfEdges,3)*Math.sqrt(2))/12;
+    }
+    @Override
+    public double getArea(){
+        return Math.sqrt(3) * Math.pow(lengthOfEdges,2);
+    }
+
 }
