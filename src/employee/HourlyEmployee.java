ca09dd03fb5727a4bfee6afea78c8cedaabb46dc
Philip <ajibolaphilip10@gmail.com>
2024-02-27 22:12:57 -0800

    update

---
+++ src/employee/HourlyEmployee.java
@@ -0,0 +1,38 @@
+package employee;
+
+public class HourlyEmployee extends Employee{
+    private double wages;
+    private int hour;
+    public HourlyEmployee(String firstName, String lastName, String socialSecurityNumber,double wages,int hour) {
+        super(firstName, lastName, socialSecurityNumber);
+        if(wages<=0.0)throw new IllegalArgumentException("wages must be greater than 0.0");
+        if(hour<=0 || hour>168) throw new IllegalArgumentException("hour must be greater than 0");
+    }
+
+    @Override
+    public double getPayment() {
+        if(hour<40) return getWages()*getHour();
+        else return 40 * getWages() + (getHour() - 40) * getWages() * 1.5;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        if(hour<=0 || hour>168) throw new IllegalArgumentException("hour must be greater than 0");
+        this.hour = hour;
+    }
+
+    public double getWages() {
+        if(wages<=0.0)throw new IllegalArgumentException("wages must be greater than 0.0");
+        return wages;
+    }
+
+    public void setWages(double wages) {
+        this.wages = wages;
+    }
+    public String toString(){
+        return String.format("Hourly Employee \n%s \n Hour of Work: %d \n wages: %.2f",super.toString(),getHour(),getWages());
+    }
+}
--- src/employee/SalariedEmployee.java
+++ src/employee/SalariedEmployee.java
@@ -4,11 +4,23 @@ public class SalariedEmployee extends Employee{
     private double weeklySalary;
     public SalariedEmployee(String firstName, String lastName, String socialSecurityNumber,double weeklySalary) {
         super(firstName, lastName, socialSecurityNumber);
-        if(weeklySalary<0.0) throw new IllegalArgumentException("weekly salary must be grater than 0.0");
+        if(weeklySalary<=0.0) throw new IllegalArgumentException("weekly salary must be grater than 0.0");
     }
 
     @Override
     public double getPayment() {
-        return 0;
+        return getWeeklySalary();
+    }
+
+    public void setWeeklySalary(double weeklySalary) {
+        if(weeklySalary<=0.0) throw new IllegalArgumentException("weekly salary must be grater than 0.0");
+        this.weeklySalary = weeklySalary;
+    }
+
+    public double getWeeklySalary() {
+        return weeklySalary;
+    }
+    public String toString(){
+        return String.format("SalariedEmployee: \n%s \nWeeklySalary: %.2f",super.toString(),getWeeklySalary());
     }
 }
