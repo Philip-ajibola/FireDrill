1eae9e69e0b47c5d7fe316c9705da01206688555
Philip <ajibolaphilip10@gmail.com>
2024-02-27 23:55:16 -0800

    update

--- src/employee/CommisionEmployee.java
+++ src/employee/CommisionEmployee.java
@@ -14,21 +14,17 @@ public class CommisionEmployee extends Employee{
     public double getCommissionRate() {
         return commissionRate;
     }
-
     public void setCommissionRate(double commissionRate) {
         if (commissionRate <= 0.0 || commissionRate >= 1.0) throw new IllegalArgumentException("Commission rate must be > 0.0 and < 1.0");
         this.commissionRate = commissionRate;
     }
-
     @Override
     public double getPayment() {
         return getCommissionRate() * getGrossSales();
     }
-
     public double getGrossSales() {
         return grossSales;
     }
-
     public void setGrossSales(double grossSales) {
         if (grossSales < 0.0) throw new IllegalArgumentException("Gross sales must be >= 0.0");
         this.grossSales = grossSales;
