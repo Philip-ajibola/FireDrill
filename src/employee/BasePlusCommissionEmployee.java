2fe7ffdf38ab1eee619955b6b78d7e7c83cec6db
Philip <ajibolaphilip10@gmail.com>
2024-02-27 22:51:05 -0800

    update

--- src/employee/BasePlusCommissionEmployee.java
+++ src/employee/BasePlusCommissionEmployee.java
@@ -5,6 +5,22 @@ public class BasePlusCommissionEmployee extends CommisionEmployee {
     public BasePlusCommissionEmployee(String firstName, String lastName, String socialSecurityNumber, double grossSales, double commissionRate) {
         super(firstName, lastName, socialSecurityNumber, grossSales, commissionRate);
         if (baseSalary < 0.0) throw new IllegalArgumentException("Base salary must be >= 0.0");
-         this.baseSalary = baseSalary;
+        this.baseSalary = baseSalary;
+    }
+
+    @Override
+    public double getPayment() {
+        return super.getPayment() + getBaseSalary();
+    }
+
+    public double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+    public String toString(){
+        return String.format("BasePlusCommissionEmployee: \n %s \n %.2f",super.toString(),getPayment());
     }
 }
