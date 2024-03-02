4a2112dc795afda65d3d78146a49cfa326722bdf
Philip <ajibolaphilip10@gmail.com>
2024-03-02 14:57:29 -0800

    update

--- src/ticTacTo/TicTacTo.java
+++ src/ticTacTo/TicTacTo.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 
 public class TicTacTo {
-    private final Value[][] VALUES = new Value[3][3];
+    private final Value[][] BOARD = new Value[3][3];
     private Player[] players = new Player[2];
     public TicTacTo(){
         fillValuesWithEmpty();
@@ -15,7 +15,7 @@ public class TicTacTo {
     }
 
     private void fillValuesWithEmpty() {
-        for (Value[] value : VALUES) {
+        for (Value[] value : BOARD) {
             Arrays.fill(value, Value.EMPTY);
         }
     }
@@ -27,24 +27,28 @@ public class TicTacTo {
     }
     private void checkInputThatFitInForRow1(Player player,int input){
         if(input >0 && input<=3){
-            if(input ==1) VALUES[0][0] = Value.X;
-            else if(input == 2)VALUES[0][1] = Value.X;
-            else VALUES[0][2] = Value.X;
+            if(input ==1) BOARD[0][0] = Value.X;
+            else if(input == 2) BOARD[0][1] = Value.X;
+            else BOARD[0][2] = Value.X;
         }
     }
     private void checkInputThatFitInForRow2(int input){
         if(input >3 && input<=6){
-            if(input == 4) VALUES[1][0] = Value.X;
-            else if(input == 5)VALUES[1][1] = Value.X;
-            else VALUES[1][2] = Value.X;
+            if(input == 4) BOARD[1][0] = Value.X;
+            else if(input == 5) BOARD[1][1] = Value.X;
+            else BOARD[1][2] = Value.X;
         }
     }
     private void checkInputThatFitInForRow3(int input){
         if(input >6 && input<=9){
-            if(input == 7) VALUES[2][0] = Value.X;
-            else if(input == 8)VALUES[2][1] = Value.X;
-            else VALUES[2][2] = Value.X;
+            if(input == 7) BOARD[2][0] = Value.X;
+            else if(input == 8) BOARD[2][1] = Value.X;
+            else BOARD[2][2] = Value.X;
         }
+    }
+
+    public Value[][] getBoard() {
+        return BOARD;
     }
 }
 
--- test/ticTacTo/TicTacToTest.java
+++ test/ticTacTo/TicTacToTest.java
@@ -1,7 +1,20 @@
 package ticTacTo;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TicTacToTest {
 
+    @Test
+    public void testThatElementInBoardIsEmpty(){
+        TicTacTo ticTacTo = new TicTacTo();
+        Value[][] board = ticTacTo.getBoard();
+        Value[][] expectedBoard = {
+                {Value.EMPTY,Value.EMPTY,Value.EMPTY},
+                {Value.EMPTY,Value.EMPTY,Value.EMPTY},
+                {Value.EMPTY,Value.EMPTY,Value.EMPTY}
+        };
+        assertArrayEquals(expectedBoard,board);
+    }
 }
