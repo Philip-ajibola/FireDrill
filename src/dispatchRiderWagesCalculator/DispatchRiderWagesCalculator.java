3e2e912859db0223e1ee6db7b40943a8b1d47ae8
Philip <ajibolaphilip10@gmail.com>
2024-03-07 00:21:47 -0800

    update

--- src/dispatchRiderWagesCalculator/DispatchRiderWagesCalculator.java
+++ src/dispatchRiderWagesCalculator/DispatchRiderWagesCalculator.java
@@ -10,7 +10,7 @@ public class DispatchRiderWagesCalculator {
     private static int checkNumberOfDeliveryMade(int numberOfDeliveryMade){
         if(numberOfDeliveryMade<50) return 5000 + (numberOfDeliveryMade *160);
         if(numberOfDeliveryMade>50 && numberOfDeliveryMade<60) return  5000 + (numberOfDeliveryMade * 200);
-        if(numberOfDeliveryMade>60 && numberOfDeliveryMade<67) return 5000 + (numberOfDeliveryMade * 250);
+        if(numberOfDeliveryMade>60 && numberOfDeliveryMade<70) return 5000 + (numberOfDeliveryMade * 250);
         else  return 5000 + (numberOfDeliveryMade * 500);
     }
     private static void validateNumberOfDelivery(int numberOfDelivery){
