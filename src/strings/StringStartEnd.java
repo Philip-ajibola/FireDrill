04c3f0d9bd317076301219d69570903ed1743589
Philip <ajibolaphilip10@gmail.com>
2024-03-09 00:17:32 -0800

    update

--- src/strings/StringStartEnd.java
+++ src/strings/StringStartEnd.java
@@ -1,5 +1,26 @@
 package strings;
 
 public class StringStartEnd {
-
+    public static void main(String[] args) {
+        String[] strings = {"started","starting","ended","ending"};
+        for(String string: strings){
+            if(string.startsWith("st")) System.out.printf("%s start with \"st\" %n",string);
+        }
+        System.out.println();
+        for(String string: strings){
+            if(string.startsWith("st")) System.out.printf("%s start with \"st\" %n",string);
+        }
+        System.out.println();
+        for(String string : strings){
+            if(string.startsWith("art",2)) System.out.printf("%s Start with \"art\" at position two \n",string);
+        }
+        System.out.println();
+        for(String string : strings){
+            if(string.endsWith("ed")) System.out.printf("%s end with \"ed\" \n",string);
+        }
+        System.out.println();
+        for(String string : strings){
+            if(string.endsWith("d")) System.out.printf("%s end with \"d\" \n",string);
+        }
+    }
 }
