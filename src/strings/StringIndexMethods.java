ad281c48b955ce9c876a0bfe6ee4dbc00d9cffd8
Philip <ajibolaphilip10@gmail.com>
2024-03-09 01:04:19 -0800

    update

--- src/strings/StringIndexMethods.java
+++ src/strings/StringIndexMethods.java
@@ -4,14 +4,15 @@ public class StringIndexMethods {
     public static void main(String[] args) {
         String letters = "abcdefghijklmabcdefghijklm";
         System.out.printf("'c' is located at index %d%n",letters.indexOf('c'));
-        System.out.printf("'c' is located at index %d%n",letters.indexOf('a',1));
-        System.out.printf("'c' is located at index %d%n",letters.indexOf('$'));
+        System.out.printf("'a' is located at index %d%n",letters.indexOf('a',1));
+        System.out.printf("'$' is located at index %d%n",letters.indexOf('$'));
 
-        System.out.printf("last c appears at index %d",letters.lastIndexOf("c"));
-        System.out.printf("last c appears at index %d",letters.lastIndexOf("a",25));
-        System.out.printf("last c appears at index %d",letters.lastIndexOf("$"));
+        System.out.printf("\nlast c appears at index %d\n",letters.lastIndexOf("c"));
+        System.out.printf("last a appears at index %d\n",letters.lastIndexOf("a",25));
+        System.out.printf("last $ appears at index %d\n",letters.lastIndexOf("$"));
 
-        System.out.printf("\"def\" is located at index %d",letters.lastIndexOf("def"));
-        System.out.printf("\"def\" is located at index %d",letters.lastIndexOf("def",7));
+        System.out.printf("\n\"def\" is located at index %d\n",letters.lastIndexOf("def"));
+        System.out.printf("\"def\" is located at index %d\n",letters.lastIndexOf("def",7));
+        System.out.printf("\"def\" is located at index %d\n",letters.lastIndexOf("def",25));
     }
 }
