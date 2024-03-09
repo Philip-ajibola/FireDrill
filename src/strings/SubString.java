a33d0f78215a5dadae5449418987296a3b4db6bf
Philip <ajibolaphilip10@gmail.com>
2024-03-09 01:11:14 -0800

    update

--- src/strings/SubString.java
+++ src/strings/SubString.java
@@ -1,4 +1,8 @@
 package strings;
 
 public class SubString {
+    public static void main(String[] args) {
+        String letters = "abcdefghijklmabcdefghijklm";
+        System.out.println("sub string from index 20 to end is " + letters.substring(20));
+    }
 }
