eee182bbdc9cb565fa4466cea538682ae658c4c1
Philip <ajibolaphilip10@gmail.com>
2024-03-09 01:13:30 -0800

    update

--- src/strings/SubString.java
+++ src/strings/SubString.java
@@ -4,5 +4,7 @@ public class SubString {
     public static void main(String[] args) {
         String letters = "abcdefghijklmabcdefghijklm";
         System.out.println("sub string from index 20 to end is " + letters.substring(20));
+        System.out.println();
+        System.out.println("sub string from index 3 up to, but not up to index 6  is " + letters.substring(3,6));
     }
 }
