d977e5d8f03d62fd26a316f0bb2d972cbaa9d4aa
Philip <ajibolaphilip10@gmail.com>
2024-03-12 16:51:38 -0700

    update

---
+++ src/AirConditioner.java
@@ -0,0 +1,28 @@
+public class AirConditioner {
+    private boolean isOn;
+    private int temperature = 16;
+
+    public boolean isOn() {
+        return isOn;
+    }
+
+    public void toggle() {
+        isOn = !isOn;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public void increaseTemperature() {
+        boolean temperatureIsWithinRange = temperature >= 16 && temperature < 30;
+        if (isOn && temperatureIsWithinRange) temperature++;
+    }
+
+    public void decreaseTemperature() {
+        boolean temperatureIsWithinRange = temperature > 16 && temperature <= 30;
+        if (isOn && temperatureIsWithinRange) temperature--;
+    }
+
+
+}
--- test/AirConditionalTest.java
+++ test/AirConditionalTest.java
@@ -1,31 +1,78 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.concurrent.locks.Condition;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AirConditionalTest {
-    @Test
-    public  void testIfAirConditionalClassExist() {
-        AirConditional airConditional = new AirConditional();
-        assertNotNull(airConditional);
-    }
-    @Test
-   public  void isTurnOn() {
-        AirConditional airConditional = new AirConditional();
-        boolean condition = airConditional.isOn();
-        assertFalse(condition);
-        airConditional.OnAirCondition();
-        boolean condition1 = airConditional.isOn();
-        assertTrue(condition1);
+    private AirConditioner airConditioner;
+
+    @BeforeEach
+    public void initializeAc() {
+        airConditioner = new AirConditioner();
     }
 
     @Test
-    public void isTurnOff(){
-        AirConditional airConditional = new AirConditional();
-        boolean condition = airConditional.isTurnOff();
-        assertFalse(condition);
-        airConditional.OffAirConditional();
-        condition = airConditional.isTurnOff();
+    public void turnOnAc_acIsOnTest() {
+        assertFalse(airConditioner.isOn());
+
+        airConditioner.toggle();
+        assertTrue(airConditioner.isOn());
     }
+
+    @Test
+    public void turnOffAc_acIsOffTest() {
+        assertFalse(airConditioner.isOn());
+        airConditioner.toggle();
+        assertTrue(airConditioner.isOn());
+
+        airConditioner.toggle();
+        assertFalse(airConditioner.isOn());
+    }
+
+    @Test
+    public void increaseAcTemperatureFrom16To17_temperatureIncreasesTest() {
+        airConditioner.toggle();
+        assertTrue(airConditioner.isOn());
+        assertEquals(16, airConditioner.getTemperature());
+
+        airConditioner.increaseTemperature();
+        assertEquals(17, airConditioner.getTemperature());
+    }
+
+    @Test
+    public void decreaseAcTemperatureFrom18To17_temperatureDecreasesTest() {
+        airConditioner.toggle();
+        assertTrue(airConditioner.isOn());
+        airConditioner.increaseTemperature();
+        airConditioner.increaseTemperature();
+        assertEquals(18, airConditioner.getTemperature());
+
+
+        airConditioner.decreaseTemperature();
+        assertEquals(17, airConditioner.getTemperature());
+    }
+
+    @Test
+    public void increaseAcTemperatureAt30_temperatureIs30Test() {
+        airConditioner.toggle();
+        assertTrue(airConditioner.isOn());
+        assertEquals(16, airConditioner.getTemperature());
+
+        for (int count = 0; count < 14; count++) airConditioner.increaseTemperature();
+        assertEquals(30, airConditioner.getTemperature());
+
+        airConditioner.increaseTemperature();
+        assertEquals(30, airConditioner.getTemperature());
+    }
+
+    @Test
+    public void decreaseAcTemperatureAt16_temperatureIs16Test() {
+        airConditioner.toggle();
+        assertTrue(airConditioner.isOn());
+        assertEquals(16, airConditioner.getTemperature());
+
+        airConditioner.decreaseTemperature();
+        assertEquals(16, airConditioner.getTemperature());
+    }
+
 }
