5d0d75a19b6376f149f084a955149d7b71248500
Philip <ajibolaphilip10@gmail.com>
2024-03-11 20:31:48 -0700

    update

--- src/estore/Customer.java
+++ src/estore/Customer.java
@@ -1,6 +1,7 @@
 package estore;
 
 public class Customer extends User{
+    private ShoppingCart shoppingCart;
 
 
     public Customer(String name, String homeAddress, String emailAddress, String phoneNumber, String password, int age) {
@@ -11,5 +12,8 @@ public class Customer extends User{
     public void resetPassword(String oldPassword, String newPassword) {
         if(!super.getPassword().equals(oldPassword)) throw new WrongPasswordException("Wrong password");
         super.setPassword(newPassword);
+    }
+
+    public void addItemToCart(Item item) {
     }
 }
---
+++ src/estore/ShoppingCart.java
@@ -0,0 +1,10 @@
+package estore;
+
+import java.util.ArrayList;
+
+public class ShoppingCart {
+    private ArrayList<Item>items;
+    public ShoppingCart(){
+        items = new ArrayList<>();
+    }
+}
