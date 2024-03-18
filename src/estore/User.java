2375f0f2c5a52871c340610d88c7b5f47052cea3
Philip <ajibolaphilip10@gmail.com>
2024-03-17 23:42:23 -0700

    update

--- src/estore/User.java
+++ src/estore/User.java
@@ -2,14 +2,14 @@ package estore;
 
 public abstract class User {
     private String name;
-    private String homeAddress;
+    private Address homeAddress;
     private String emailAddress;
     private String phoneNumber;
     private String password;
     private int age;
 
 
-    public User(String name, String homeAddress, String emailAddress, String phoneNumber, String password, int age){
+    public User(String name, Address homeAddress, String emailAddress, String phoneNumber, String password, int age){
             this.name = name;
             this.homeAddress = homeAddress;
             this.emailAddress =emailAddress;
