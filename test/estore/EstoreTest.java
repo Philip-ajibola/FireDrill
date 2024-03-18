c613ccbc64196863825e874bbe28e835c36f1d55
Philip <ajibolaphilip10@gmail.com>
2024-03-17 23:40:29 -0700

    update

--- test/estore/EstoreTest.java
+++ test/estore/EstoreTest.java
@@ -1,65 +1,115 @@
 package estore;
 
+import estore.Exception.InValidCardType;
+import estore.Exception.WrongPasswordException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class EstoreTest {
+    private  Admin admin;
+    private Address adminAddress;
+    @BeforeEach
+    public void creatingAdmin(){
+        admin = new Admin("name",adminAddress,"ajibola@gmail.com","09027531222","passwword",15);
+        adminAddress = new Address("Ikeja","Nigeria","23","yaba","Lagos");
+    }
     @Test
     public void testThatUserCanBeAddedToEstore(){
         Estore estore = new Estore();
-        estore.addSeller("name","10 Yaba road","ajibola@gmail.com","09027531222","passwword",15);
+        Address sellerAddress = new Address("Ikeja","Nigeria","23","yaba","Lagos");
+        Seller seller = new Seller("name",sellerAddress,"ajibola@gmail.com","09027531222","passwword",15);
+        admin.addSeller(seller,estore);
         assertEquals(1,estore.getSellers().size());
     }
     @Test
     public void testThatSellerCanBeFoundBySellerPhoneNumber(){
         Estore estore = new Estore();
-        estore.addSeller("name","10 Yaba road","ajibola@gmail.com","09027531222","passwword",15);
-        Seller seller = estore.findSellerWithSellerPhoneNumber("09027531222");
-            assertEquals(seller,estore.getSellers().get(0));
+        Address sellerAddress = new Address("Ikeja","Nigeria","23","yaba","Lagos");
+        Seller seller = new Seller("name",sellerAddress,"ajibola@gmail.com","09027531222","passwword",15);
+
+        admin.addSeller(seller,estore);
+        Seller sellerExpected = admin.findSeller(seller,estore);
+            assertEquals(seller,sellerExpected);
     }
     @Test
     public void testThatSellerCanResetPassword(){
-        Seller seller = new Seller("name","10 Yaba road","ajibola@gmail.com","09027531222","password",15);
+        Address sellerAddress = new Address("Ikeja","Nigeria","23","yaba","Lagos");
+        Seller seller = new Seller("name",sellerAddress,"ajibola@gmail.com","09027531222","password",15);
+
         seller.resetPassword("password","newPassword");
         assertEquals("newPassword",seller.getPassword());
     }
     @Test
     public void testThatWhenUserWantToResetPasswordWithWrongOldPassword_errorIsThrown(){
-        Seller seller = new Seller("name","10 Yaba road","ajibola@gmail.com","09027531222","password",15);
+        Address sellerAddress = new Address("Ikeja","Nigeria","23","yaba","Lagos");
+
+        Seller seller = new Seller("name",sellerAddress,"ajibola@gmail.com","09027531222","password",15);
         assertThrows(WrongPasswordException.class,()-> seller.resetPassword("oldPassword","newPassword"));
     }
     @Test
     public void testThatSellerCanAddItemToListOfItem(){
+        Address sellerAddress = new Address("Ikeja","Nigeria","23","yaba","Lagos");
         Estore estore = new Estore();
-        estore.addSeller("name","10 Yaba road","ajibola@gmail.com","09027531222","passwword",15);
-        Seller seller = estore.findSellerWithSellerPhoneNumber("09027531222");
+        Seller seller = new Seller("name",sellerAddress,"ajibola@gmail.com","09027531222","password",15);
+
+        Seller seller1 = estore.findSeller(seller);
         Product product = new Product( 1, "productName",45.0,ProductCategory.ELECTRONICS,"description");
-        seller.addProduct(estore,product);
+        seller.addProduct(admin,product,estore);
         assertEquals(product,estore.getProducts("productName"));
+
+    }
+    @Test
+    public void testThatWhenProductDoesNotExistInProductListExceptionIsThrown(){
+        Address sellerAddress = new Address("Ikeja","Nigeria","23","yaba","Lagos");
+        Estore estore = new Estore();
+        Seller seller = new Seller("name",sellerAddress,"ajibola@gmail.com","09027531222","password",15);
+
+        Seller seller1 = estore.findSeller(seller);
+        Product product = new Product( 1, "productName",45.0,ProductCategory.ELECTRONICS,"description");
+        seller.addProduct(admin,product,estore);
+        assertThrows(ProductNotFoundException.class,()->admin.getProducts("productname",estore));
 
     }
     @Test
     public void testThatSellerCanRemoveProductFromListOfProduct(){
+        Address sellerAddress = new Address("Ikeja","Nigeria","23","yaba","Lagos");
         Estore estore = new Estore();
-        estore.addSeller("name","10 Yaba road","ajibola@gmail.com","09027531222","passwword",15);
-        Seller seller = estore.findSellerWithSellerPhoneNumber("09027531222");
+        Seller seller = new Seller("name",sellerAddress,"ajibola@gmail.com","09027531222","password",15);
         Product product = new Product( 1, "productName",45.0,ProductCategory.ELECTRONICS,"description");
-        seller.addProduct(estore,product);
+        seller.addProduct(admin,product,estore);
         assertEquals(product,estore.getProducts("productName"));
-        seller.removeProduct(estore,"productName");
-        assertEquals(0,estore.getListOfProduct().size());
+        seller.removeProduct(admin,"productName",estore);
+        assertEquals(0,estore.getListOfProduct());
 
     }
     @Test
     public void testThatCustomerCanCreateAccount(){
+        Address customerAddress = new Address("Ikeja","Nigeria","23","yaba","Lagos");
         Estore estore = new Estore();
-        estore.addCustomer("name","10 Yaba road","ajibola@gmail.com","09027531222","passwword",15);
-        assertEquals(1,estore.getCustomers().size());
+        CreditCardInformation creditCardInformation = new CreditCardInformation("ccV","4132456378124356");
+        BillingFormat billingFormat = new BillingFormat("phoneNumber","name",customerAddress,creditCardInformation);
+        Customer customer = new Customer("name",customerAddress,"ajibola@gmail.com","09027531222","passwword",15,billingFormat);
+        admin.addCustomer(customer,estore);
+        assertEquals(1,estore.getListCustomers());
+    }
+    @Test
+    public void testThatWhenCustomerWantToResetPasswordWithWrongOldPassword_errorIsThrown(){
+        Address customerAddress = new Address("Ikeja","Nigeria","23","yaba","Lagos");
+        CreditCardInformation creditCardInformation = new CreditCardInformation("ccV","4132456378124356");
+
+        BillingFormat billingFormat = new BillingFormat("phoneNumber","name",customerAddress,creditCardInformation);
+
+        Customer customer = new Customer("name",customerAddress,"ajibola@gmail.com","09027531222","password",15,billingFormat);
+        assertThrows(WrongPasswordException.class,()-> customer.resetPassword("oldPassword","newPassword"));
     }
     @Test
     public void testThatCustomerCanAddItemsToShoppingCart(){
-        Customer customer = new Customer("name","10 Yaba road","ajibola@gmail.com","09027531222","passwword",15);
+        Address customerAddress = new Address("Ikeja","Nigeria","23","yaba","Lagos");
+        CreditCardInformation creditCardInformation = new CreditCardInformation("ccV","4132456378124356");
+        BillingFormat billingFormat = new BillingFormat("phoneNumber","name",customerAddress,creditCardInformation);
+        Customer customer = new Customer("name",customerAddress,"ajibola@gmail.com","09027531222","passwword",15,billingFormat);
         Product product = new Product( 1, "productName",45.0,ProductCategory.ELECTRONICS,"description");
         Item item = new Item(product,2);
         customer.addItemToCart(item);
@@ -67,7 +117,10 @@ class EstoreTest {
     }
     @Test
     public void testThatCustomerCanAddDifferentItemsToShoppingCart(){
-        Customer customer = new Customer("name","10 Yaba road","ajibola@gmail.com","09027531222","passwword",15);
+        Address customerAddress = new Address("Ikeja","Nigeria","23","yaba","Lagos");
+        CreditCardInformation creditCardInformation = new CreditCardInformation("ccV","4132456378124356");
+        BillingFormat billingFormat = new BillingFormat("phoneNumber","name",customerAddress,creditCardInformation);
+        Customer customer = new Customer("name",customerAddress,"ajibola@gmail.com","09027531222","passwword",15,billingFormat);
         Product product = new Product( 1, "productName",45.0,ProductCategory.ELECTRONICS,"description");
         Product product1 = new Product( 1, "productName",30,ProductCategory.GROCERIES,"description");
         Item item = new Item(product,2);
@@ -78,7 +131,10 @@ class EstoreTest {
     }
     @Test
     public void testThatCustomerCanRemoveItemFromShoppingCart(){
-        Customer customer = new Customer("name","10 Yaba road","ajibola@gmail.com","09027531222","passwword",15);
+        Address customerAddress = new Address("Ikeja","Nigeria","23","yaba","Lagos");
+        CreditCardInformation creditCardInformation = new CreditCardInformation("ccV","4132456378124356");
+        BillingFormat billingFormat = new BillingFormat("phoneNumber","name",customerAddress,creditCardInformation);
+        Customer customer = new Customer("name",customerAddress,"ajibola@gmail.com","09027531222","passwword",15,billingFormat);
         Product product = new Product( 1, "productName",45.0,ProductCategory.ELECTRONICS,"description");
         Product product1 = new Product( 1, "productName",30,ProductCategory.GROCERIES,"description");
         Item item = new Item(product,2);
@@ -88,5 +144,32 @@ class EstoreTest {
         customer.removeItem(item);
         assertEquals(1,customer.getItems().size());
     }
+    @Test
+    public void testThatCustomerCanPayForTheItemsTheyOrderFor(){
+        Address customerAddress = new Address("Ikeja","Nigeria","23","yaba","Lagos");
+        CreditCardInformation creditCardInformation = new CreditCardInformation("ccV","4132456378124356");
+        BillingFormat billingFormat = new BillingFormat("phoneNumber","name",customerAddress,creditCardInformation);
+
+        Customer customer = new Customer("name",customerAddress,"ajibola@gmail.com","09027531222","passwword",15,billingFormat);
+        Product product = new Product( 1, "productName",45.0,ProductCategory.ELECTRONICS,"description");
+        Product product1 = new Product( 1, "productName",30,ProductCategory.GROCERIES,"description");
+        Item item = new Item(product,2);
+        Item item1 = new Item(product1,6);
+        customer.addItemToCart(item);
+        customer.addItemToCart(item1);
+        Order order = new Order(customer);
+        assertEquals(2,customer.getItems().size());
+        assertEquals(270,customer.order(order));
+    }
+    @Test
+    public void testThatWhenInValidCardIsUsedExceptionISThrown(){
+        assertThrows(InValidCardType.class,()-> new CreditCardInformation("ccV","7132456378124356"));
+    }
+    @Test
+    public void testThatIfTheLengthOfTheCardNumberIsNot13Or16ErrorIsThrown(){
+        assertThrows(InValidCardType.class,()-> new CreditCardInformation("ccV","45637812435678"));
+        CreditCardInformation creditCardInformation = new CreditCardInformation("ccV","4132456378124356");
+    }
+
 
 }
